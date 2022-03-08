b5acb9496eefd735bcd23b88e038d45eaad16100
senyuanzi <senyuanzi@outlook.com>
2022-03-08 13:38:17 +0800

    Api : repeat 快速复制多个 用来快速看效果等场景

---
+++ lib/ui/collection.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:ones/ui/scrollable.dart';
+
+extension ScrollableX<T extends Widget> on T {
+  SingleChildScrollView repeat(int number, {Axis direction = Axis.vertical}) {
+    var list = <T>[];
+    for (int i = 0; i < number; i++) {
+      list.add(this);
+    }
+
+    return [...list].scrollable(direction: direction);
+  }
+}
--- lib/ui/scrollable.dart
+++ lib/ui/scrollable.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:ones/ui.dart';
 
 extension ScrollableX<T extends Widget> on T {
-  SingleChildScrollView scrollable(Axis direction) {
+  SingleChildScrollView scrollable({Axis direction = Axis.vertical}) {
     return SingleChildScrollView(scrollDirection: direction, child: this);
   }
 }
 
 extension MultiWidgetScrollableX<T extends List> on T {
-  SingleChildScrollView scrollable(Axis direction) {
+  SingleChildScrollView scrollable({Axis direction = Axis.vertical}) {
     return SingleChildScrollView(
       scrollDirection: direction,
       child: direction == Axis.vertical ? Column(children: [...this]) : Row(children: [...this]),
